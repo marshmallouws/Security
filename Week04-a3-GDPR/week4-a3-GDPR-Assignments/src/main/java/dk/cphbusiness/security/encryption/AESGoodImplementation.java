922501d2210962760e4a810d665cb3c5e2a6413e
Annika Ehlers <e_hlers@live.dk>
2020-06-11 21:43:51 +0200

    small fix

--- Week04-a3-GDPR/week4-a3-GDPR-Assignments/src/main/java/dk/cphbusiness/security/encryption/AESGoodImplementation.java
+++ Week04-a3-GDPR/week4-a3-GDPR-Assignments/src/main/java/dk/cphbusiness/security/encryption/AESGoodImplementation.java
@@ -16,43 +16,35 @@ import java.util.Base64;
 /*
  Code borrowed from https://stackoverflow.com/questions/15554296/simple-java-aes-encrypt-decrypt-example
  example provided by k170.
-*/
-public class AESGoodImplementation 
-{
-    
-    public static byte[] encrypt(final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws InvalidKeyException, InvalidAlgorithmParameterException
-    {       
+ */
+public class AESGoodImplementation {
+
+    public static byte[] encrypt(final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws InvalidKeyException, InvalidAlgorithmParameterException {
         return AESGoodImplementation.transform(Cipher.ENCRYPT_MODE, keyBytes, ivBytes, messageBytes);
     }
 
-    public static byte[] decrypt(final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws InvalidKeyException, InvalidAlgorithmParameterException
-    {       
+    public static byte[] decrypt(final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws InvalidKeyException, InvalidAlgorithmParameterException {
         return AESGoodImplementation.transform(Cipher.DECRYPT_MODE, keyBytes, ivBytes, messageBytes);
     }
 
-    private static byte[] transform(final int mode, final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws InvalidKeyException, InvalidAlgorithmParameterException
-    {
+    private static byte[] transform(final int mode, final byte[] keyBytes, final byte[] ivBytes, final byte[] messageBytes) throws InvalidKeyException, InvalidAlgorithmParameterException {
         final SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
         final IvParameterSpec ivSpec = new IvParameterSpec(ivBytes);
         byte[] transformedBytes = null;
 
-        try
-        {
+        try {
             final Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
 
             cipher.init(mode, keySpec, ivSpec);
 
             transformedBytes = cipher.doFinal(messageBytes);
-        }        
-        catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) 
-        {
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException | BadPaddingException e) {
             e.printStackTrace();
         }
         return transformedBytes;
     }
 
-    public static void main(final String[] args) throws InvalidKeyException, InvalidAlgorithmParameterException
-    {
+    public static void main(final String[] args) throws InvalidKeyException, InvalidAlgorithmParameterException {
         //Retrieved from a protected local file.
         //Do not hard-code and do not version control.
         final String base64Key = "ABEiM0RVZneImaq7zN3u/w==";
@@ -62,7 +54,7 @@ public class AESGoodImplementation
         final String storedCifer = "AAECAwQFBgcICQoLDA0ODw==:ZtrkahwcMzTu7e/WuJ3AZmF09DE=";
 
         //Extract the iv and the ciphertext from the shadow entry.
-        final String[] shadowData = storedCifer.split(":");        
+        final String[] shadowData = storedCifer.split(":");
         final String base64Iv = shadowData[0];
         final String base64Ciphertext = shadowData[1];
 
@@ -74,13 +66,13 @@ public class AESGoodImplementation
         //Decrypt data and do something with it.
         final byte[] decryptedBytes = AESGoodImplementation.decrypt(keyBytes, ivBytes, encryptedBytes);
         final String decryptedMsg = new String(decryptedBytes, StandardCharsets.UTF_8);
-        
+
         //Use non-blocking SecureRandom implementation for the new IV.
         final SecureRandom secureRandom = new SecureRandom();
 
         //Generate a new IV.
         secureRandom.nextBytes(ivBytes);
-        String newStoredCipher = Base64.getEncoder().encodeToString(ivBytes) 
+        String newStoredCipher = Base64.getEncoder().encodeToString(ivBytes)
                 + ":" + Base64.getEncoder().encodeToString(AESGoodImplementation.encrypt(keyBytes, ivBytes, decryptedBytes));
 
         //At this point instead of printing to the screen, 
